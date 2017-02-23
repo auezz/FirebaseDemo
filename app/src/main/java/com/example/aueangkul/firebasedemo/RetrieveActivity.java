59fb79e98dea71b4f4c4647d73ac2c690ba868b9
aueangkul.su.56 <aueangkul.su.56@ubu.ac.th>
2017-02-23 17:14:31 +0700

    edit 23/02/17

--- app/src/main/java/com/example/aueangkul/firebasedemo/RetrieveActivity.java
+++ app/src/main/java/com/example/aueangkul/firebasedemo/RetrieveActivity.java
@@ -3,6 +3,7 @@ package com.example.aueangkul.firebasedemo;
 import android.content.Context;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,7 +13,6 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -20,155 +20,352 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 
 public class RetrieveActivity extends AppCompatActivity {
-    List<List<Long>> test = new ArrayList<List<Long>>();
-
+    List<Test> test;
+    List<String> sss = new ArrayList<String>();
     DatabaseReference dref;
     ListView rtList;
     ArrayList<String> list = new ArrayList<>();
-    ArrayList<Object> list2 = new ArrayList<>();
     ArrayAdapter<String> adapter;
-    TextView listT;
-    String CPosition[];
 
     Button chk;
     WifiManager wifi;
     List<Long> getRssi = new ArrayList<>();
+
     double result;
+    double result2 = 0;
+    double resultSqrt;
 
     Long rssi;
     String ssid;
     String position;
-    Long getRSSI_DATA1[] = new Long[100];
-    Long getRSSI_DATA2[] = new Long[100];
-    Long getRSSI_DATA3[];
-    Long getRSSI_DATA4[];
-    Long getData[];
-    List<Long> arr = new ArrayList<>();
-    List<Integer> tmpCal = new ArrayList<>();
-    int count = 0;
+    String macAddress;
+    List<Long> arr;
+    List<Integer> my_num = new ArrayList<>();
+    List<String> AllResultSqrt = new ArrayList<>();
+    List<String> PositionList = new ArrayList<>();
+    String KPosition;
+    List<String> myPosition = new ArrayList<>();
+    int c=0;
+
+    Button Clr;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_retrieve);
 
+        test = new ArrayList<>();
+
         rtList = (ListView) findViewById(R.id.RtListView);
         adapter = new ArrayAdapter<String>(this,android.R.layout.simple_dropdown_item_1line,list);
         rtList.setAdapter(adapter);
-
+        arr = new ArrayList<>();
         //listT = (TextView) findViewById(R.id.lvTxt);
         wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
         wifi.startScan();
 
-        chk = (Button) findViewById(R.id.chkButt);
-        chk.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                if (view.getId() == R.id.chkButt){
-                    List<ScanResult> wifiScanList = wifi.getScanResults();
-                    //getData = new int[wifiScanList.size()];
-
-                    for (int i=0;i<wifiScanList.size();i++){
-                        //getData[i] = wifiScanList.get(i).level;
-                        getRssi.add((long) wifiScanList.get(i).level);
-                        //Cal(getRssi);
-
-                    }
-                    Log.e("RssiP :",getRssi+"");
-
-                }
-
-            }
-        });
-
-
-
-
         dref = FirebaseDatabase.getInstance().getReference();
-
         dref.child("messages").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
+                int count = 0;
+
                 for (DataSnapshot postSnapshot : dataSnapshot.getChildren()){
                     //Getting Data from Datasnapshot
-                    SendInformation sendInformation = postSnapshot.getValue(SendInformation.class);
-                    //DataSnapshot tm = postSnapshot.child(postSnapshot.getKey());
 
-
-                    //Adding it to a String
-                    //String string = "position"+sendInformation.getPosition()+"\nSSID "+sendInformation.getSsid()+"\nRSSI "+sendInformation.getRssi();
-
-                    //Displaying it on TextView
-//                    rtList.setAdapter(new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, string));
                     arr.clear();
                     for (DataSnapshot postSnapshots : postSnapshot.getChildren()){
-
                         position  = (String) postSnapshots.child("position").getValue();
                         ssid = (String) postSnapshots.child("ssid").getValue();
                         rssi = (Long) postSnapshots.child("rssi").getValue();
+                        macAddress = (String) postSnapshots.child("macAddress").getValue();
                         list.add(position+"  "+ssid+"  "+rssi);
+                        arr.add(rssi);
                         adapter.notifyDataSetChanged();
-                        //Log.e(position, rssi.toString());
+                    }
+
+                    test.add(new Test(arr));
+                    String str = test.get(count++).longList.toString();
+                    Log.e("Position: "+position, ""+str+" count="+count);
+                    sss.add(str);
+                    PositionList.add(position);
 
 
-                       // for(int i=0;i<arr.length;i++){
-                            //getData = rssi;
-                            arr.add(rssi);
-                            //Log.e(position+" - "+ssid+" :",arr[i].toString());
-                            //Log.e(position+" - "+ssid+" :",rssi+"");
-                            //getData = rssi;
-                       // }
-//                        //Log.e(position+" :",getData.toString());
-//                        Log.e(position+" :", Arrays.toString(arr));
-//                        count++;
-//                        Log.e("Count: ", count+"");
+                }
+
+                int count2 =1;
+                for(String strs:sss) {
+                    String tmp3 = strs.substring(1, strs.length() - 1);
+                    String[] arr2 = tmp3.split(",");
+                    int[] list_int = new int[arr2.length];
+                    int i = 0;
+                    for (String x : arr2) {
+                        list_int[i++] = Integer.parseInt(x.trim());
+                    }
+                    my_num.clear();
+                    for (int ii : list_int) {
+                        my_num.add(ii);
 
                     }
-                    Log.e(position+": ", ""+arr);
-                    test.add(arr);
-                    Log.e(test.size()+"","test");
 
-
+                    Log.e("my num: "+count2+"  Size: "+ my_num.size(), my_num + "");
+                    //Cal(my_num);
+                    Calculator(my_num);
+                    count2++;
                 }
-                List<Long> tmp;
-                for (int i=0;i<test.size();i++){
-                    tmp = test.get(i);
-                    tmpCal.add(Cal(tmp));
 
 
-                }
             }
 
-            public int Cal(List<Long> tmp){
-                //สูตร
-                //float getTmp = tmp;
-                //Long result = Math.sqrt();
 
-//                for (int i=0;i<test.size();i++){
+
+
+
+            public void Calculator(List<Integer> getData){
+
+                double upperSum;
+                double Rest =0;
+                double sqrt;
+
+                List<ScanResult> wifiScanList = wifi.getScanResults();
+                            //getData = new int[wifiScanList.size()];
+                            getRssi.clear();
+                            for (int i=0;i<wifiScanList.size();i++){
+                                getRssi.add((long) wifiScanList.get(i).level);
+                            }
+
+                Log.e("get Rssi  ",getRssi+"");
+
+
+
+               // for (int i=0;i<sss.size()-1;i++){
+                    for (int j=1;j<getRssi.size();j++){
+                        //Log.e("get RSSI  "+j+"  ", getRssi+"");
+                        upperSum = Math.pow((getRssi.get(j)-getData.get(j)),2);
+                        Rest += upperSum;
+
+                    }
+
+
+
+                    sqrt = Math.sqrt(Rest);
+//                    sortPosition.add(sqrt);
+
+
+
+                    Log.e("get data offline", getData + "");
+
+                    Log.e("Rest  "+PositionList.get(c),"  "+Rest);
+
+                    Log.e("sqrt   "+PositionList.get(c),"  "+sqrt);
+
+                   AllResultSqrt.add(PositionList.get(c)+"/"+String.format("%.2f",sqrt));
+                    //AllResultSqrt.add(sqrt);
+
+//                    String tmp[] = AllResultSqrt.spliterator("/");
+
+                    Log.e("All result  ",AllResultSqrt+"");
+
+
+//                    Collections.sort(AllResultSqrt);
+//                    Log.e("sort",AllResultSqrt+"");
+
+//                    myPosition.add(PositionList.get(c));
+//                    Log.e("myPosition", myPosition+"");
+                    c++;
+
+
+               // }
+                chk = (Button) findViewById(R.id.chkButt);
+                chk.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        AlertDialog.Builder mBuilder = new AlertDialog.Builder(RetrieveActivity.this);
+                        View mView = getLayoutInflater().inflate(R.layout.dialog, null);
+                        TextView output_Position = (TextView) mView.findViewById(R.id.Output_PositionTextView);
+                        output_Position.setText(AllResultSqrt.toString());
+//                        output_Position.setText("Hello");
+                        Log.e("All Result click", AllResultSqrt.get(0)+"");
+
+                        mBuilder.setView(mView);
+                        AlertDialog dialog = mBuilder.create();
+                        dialog.show();
+                    }
+                });
+
+            }
+
+
+
+
+
+//            public void get(List<Integer> getOffline){
 //
+//                double upperSum;
+//                double Rest =0;
+//                double sqrt;
+//
+//                List<ScanResult> wifiScanList = wifi.getScanResults();
+//                            //getData = new int[wifiScanList.size()];
+//                            getRssi.clear();
+//                            for (int i=0;i<wifiScanList.size();i++){
+//                                getRssi.add((long) wifiScanList.get(i).level);
+//                            }
+//
+////                Log.e("get Rssi  ",getRssi+"");
+//
+//
+//
+//                for (int i=0;i<sss.size()-1;i++){
+//                    for (int j=0;j<getRssi.size()-1;j++){
+//                        //Log.e("get RSSI  "+j+"  ", getRssi+"");
+//                        upperSum = Math.pow((getRssi.get(j)-getOffline.get(j)),2);
+//                        Rest += upperSum;
+//
+//                    }
+//
+//
+//
+//                    sqrt = Math.sqrt(Rest);
+////                    sortPosition.add(sqrt);
+//
+//
+//
+//                    Log.e("get data offline", getOffline + "");
+//
+//                    Log.e("Rest  "+PositionList.get(c),"  "+Rest);
+//
+//                    Log.e("sqrt   "+PositionList.get(c),"  "+sqrt);
+//
+//                   AllResultSqrt.add(PositionList.get(c)+"/"+String.format("%.2f",sqrt));
+//                    //AllResultSqrt.add(sqrt);
+//
+//                    String tmp[] = AllResultSqrt.spliterator("/");
+//
+//                    Log.e("All result  ",AllResultSqrt+"");
+//
+//
+////                    Collections.sort(AllResultSqrt);
+////                    Log.e("sort",AllResultSqrt+"");
+//
+////                    myPosition.add(PositionList.get(c));
+////                    Log.e("myPosition", myPosition+"");
+//                    c++;
 //
 //
 //                }
+//                chk = (Button) findViewById(R.id.chkButt);
+//                chk.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View view) {
+//                        AlertDialog.Builder mBuilder = new AlertDialog.Builder(RetrieveActivity.this);
+//                        View mView = getLayoutInflater().inflate(R.layout.dialog, null);
+//                        TextView output_Position = (TextView) mView.findViewById(R.id.Output_PositionTextView);
+//                        output_Position.setText(AllResultSqrt.toString());
+////                        output_Position.setText("Hello");
+//                        Log.e("All Result click", AllResultSqrt.get(0)+"");
+//
+//                        mBuilder.setView(mView);
+//                        AlertDialog dialog = mBuilder.create();
+//                        dialog.show();
+//                    }
+//                });
+//            }
 
 
 
 
 
-                return 0;
-
-            }
+//            public void Cal(final List<Integer> getOffline){
+//
+//
+//                chk = (Button) findViewById(R.id.chkButt);
+//                chk.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View view) {
+//                        getRssi.clear();
+//                        //Log.e("offline", getOffline+"");
+//                        if (view.getId() == R.id.chkButt){
+//                            List<ScanResult> wifiScanList = wifi.getScanResults();
+//                            //getData = new int[wifiScanList.size()];
+//                            for (int i=0;i<wifiScanList.size();i++){
+//                                getRssi.add((long) wifiScanList.get(i).level);
+//                            }
+//                            Log.e("RssiP :",getRssi+"");
+//
+//                        }
+//
+//
+//                        Log.e("get offline jhhgfgy.", getOffline+"");
+//
+//
+//                        for (int i=0;i<sss.size();i++){
+//                            for (int j=0;j<getRssi.size();j++) {
+//                                result = Math.pow((getRssi.get(j) - getOffline.get(j)), 2);
+//                                result2 += result;
+//                            }
+//
+//                            Log.e("tmp cal", getOffline+ "");
+//
+//                            resultSqrt = Math.sqrt(result2);
+//                            AllResultSqrt.add(resultSqrt);
+//                            Log.e("RssiP Size :"+getRssi.size(),getRssi+"");
+//
+//
+//                            //Log.e("tmp"+tmp.size(),tmp+"");
+//                            Log.e("result 2  "+i,result2+"");
+//
+//                            Log.e("Result SQRT  "+i,resultSqrt+"");
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//                        }
+//                        Collections.sort(AllResultSqrt);
+//
+//                        for (double counter: AllResultSqrt){
+//
+//                            Log.e("All result SQRT",counter+"");
+//                        }
+//
+//
+//
+//
+//
+//
+//                    }
+//                });
+//
+//
+//
+//            }
 
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
 
 
+            }
+        });
+
+        Clr = (Button) findViewById(R.id.clr_Butt);
+        Clr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dref.removeValue();
+                list.clear();
             }
         });
 
